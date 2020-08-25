387d67b76ad1a1bb53c2467f5cc47aabf9d77a81
IvnaBakovic <ibakovic@pmfst.hr>
2020-08-25 20:27:48 +0200

    Vjezbe 6 i 7

---
+++ Vjezbe_6_7/analyze.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+// My own class
+#include <Analyzer.h>
+
+using namespace std;
+
+int main()
+{
+  // Declare Analyzer
+  Analyzer *analyzer;
+
+  analyzer = new Analyzer();
+  analyzer->Loop("ggH125");
+  analyzer->Loop("qqZZ");
+  analyzer->PlotMassDkin();
+
+  delete analyzer;
+
+  return 0;
+}
